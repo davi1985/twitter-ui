c41d1b5d705e189913bcc06f705d8017852e87f3
Davi Silva <davisilvaphoto@gmail.com>
2023-03-20 09:37:28 -0300

    remove unsused lines

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -45,36 +45,6 @@ export const Sidebar = () => (
           <span>{item.label}</span>
         </a>
       ))}
-
-      {/* <a href="">
-        <Bell />
-        <span>Notifications</span>
-      </a>
-
-      <a href="">
-        <Envelope />
-        <span>Messages</span>
-      </a>
-
-      <a href="">
-        <BookmarkSimple />
-        <span>Bookmarks</span>
-      </a>
-
-      <a href="">
-        <FileText />
-        <span>Lists</span>
-      </a>
-
-      <a href="">
-        <User />
-        <span>Profile</span>
-      </a>
-
-      <a href="">
-        <DotsThreeCircle />
-        <span>More</span>
-      </a> */}
     </nav>
 
     <button className="new-tweet" type="button">
